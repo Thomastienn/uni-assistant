cea3b14e3af58cec0360d265d344a8f1e9fda0eb
thomas <thomas.vu1@ucalgary.ca>
2025-03-24 18:53:57 -0600

    Add some more

---
+++ sandbox.py
@@ -0,0 +1,11 @@
+import sys
+import math
+from fractions import Fraction
+sys.path.append("D:\\cpFiles\\uni\\helper")
+
+from polynomial import *
+from matrix import *
+from customcomplex import *
+
+a = Matrix()
+a.rref().show()
---
+++ vector.py
@@ -0,0 +1,114 @@
+from fractions import Fraction
+import math
+class Vector:
+    def __init__(self, a=None,t=Fraction):
+        self.t = t
+        if a == None:
+            self.get()
+        else:
+            self.a=a
+        
+    def get(self):
+        self.a = list(map(self.t, input().split()))
+    
+    def __add__(self,oth):
+        new = [self.a[i]+oth.a[i] for i in range(len(self.a))]
+        return Vector(a=new,t=self.t)
+    def __neg__(self):
+        return Vector(a=[-self.a[i] for i in range(len(self.a))],t=self.t)
+    def __sub__(self,o):
+        return self+(-o)
+    def __mul__(self,num):
+        if isinstance(num,Vector):
+            assert len(self.a)==len(num.a), "Not equal size"
+            return sum(self.a[i]*num.a[i] for i in range(len(self.a)))
+        new = [self.a[i]*num for i in range(len(self.a))]
+        return Vector(a=new,t=self.t)
+    def __rmul__(self,num):
+        return self*num
+    def __repr__(self):
+        return " ".join(map(str, self.a))
+    def smag(self):
+        return sum(self.a[i]**2 for i in range(len(self.a)))
+    def mag(self):
+        return self.smag()**0.5
+    def angle(self, other):
+        return math.acos((self*other)/math.sqrt(self.smag()*other.smag()))
+    def degAngle(self,other):
+        return self.angle(other)*180/math.pi
+    def piAngle(self,other):
+        cof = self.angle(other)/math.pi
+        return f"{cof:.2f}π" 
+        
+        
+class Vec3d:
+    def __init__(self,x=None,y=None,z=None,t=Fraction):
+        if(x==None):
+            self.t=t
+            self.get()
+            return
+        self.x=t(x)
+        self.y=t(y)
+        self.z=t(z)
+        self.t=t
+        
+    def get(self):
+        self.x,self.y,self.z=map(self.t,input().split())
+    
+    def __repr__(self):
+        return f"<{self.x},{self.y},{self.z}>"
+    
+    def cross(self,other):
+        new_x = self.y*other.z-self.z*other.y
+        new_y = -(self.x*other.z-self.z*other.x)
+        new_z = self.x*other.y-self.y*other.x
+        return Vec3d(new_x,new_y,new_z,t=self.t)
+        
+    def dot(self,other):
+        return Vec3d(x*other.x, y*other.y, z*other.z,t=self.t)
+    
+    def __mul__(self,num):
+        new_x = self.x*num
+        new_y = self.y*num
+        new_z = self.z*num
+        return Vec3d(new_x,new_y,new_z,t=self.t)
+    
+    def __neg__(self):
+        return Vec3d(-self.x,-self.y,-self.z,t=self.t)
+    
+    def __add__(self,other):
+        new_x = self.x+other.x
+        new_y = self.y+other.y
+        new_z = self.z+other.z
+        return Vec3d(new_x,new_y,new_z,t=self.t)
+        
+    def __sub__(self,other):
+        return self+(-other)
+        
+    def __rmul__(self,num):
+        new_x = self.x*num
+        new_y = self.y*num
+        new_z = self.z*num
+        return Vec3d(new_x,new_y,new_z,t=self.t)
+    
+    def __truediv__(self,num):
+        new_x = self.x*Fraction(1,num)
+        new_y = self.y*Fraction(1,num)
+        new_z = self.z*Fraction(1,num)
+        return Vec3d(new_x,new_y,new_z,t=self.t)
+    
+    def smag(self):
+        return self.x**2 + self.y**2 + self.z**2
+
+    def mag(self):
+        return self.smag() ** (0.5)
+
+
+u=Vec3d(-5,-1,2,t=float)
+print(u.mag()*u)
+
+
+
+
+
+
